bb0e32a0c7802eceefab1a0a2f1a72109202af33
Paul Jung <pbnjcub@gmail.com>
2022-07-06 14:26:25 -0400

    commit tree 5a483463d94149db91e6c409ca622ef006182b6a

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here
+
+function findMatching(collection, string){
+  let newString = string.toLowerCase()
+ 
+    let matchName = collection.filter(function(driver){
+       let newDriver = driver.toLowerCase()
+      return newDriver === newString
+    })
+  return matchName
+  }
+
+  function fuzzyMatch(collection, string){
+    let newString = string.toLowerCase()
+      let matchName = collection.filter(function(driver){
+        let newDriver = driver.toLowerCase()
+        for(let i = 0; i<newString.length; i++){
+          return newDriver[i] === newString[i]
+        }
+      })
+    return matchName
+    }
+
+    function matchName(collection, string){
+      let newString = string.toLowerCase()
+        let nameMatch = collection.filter(function(driver){
+          let newDriver = driver.name.toLowerCase()
+          return newDriver === newString
+          
+          })
+         return nameMatch
+        }
+
